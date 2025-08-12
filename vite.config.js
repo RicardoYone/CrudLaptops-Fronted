3346bec7f554fab7beae32cde41dd6df607da6e5
Ricardo Yone Leon Estela <ricardo.leon@gestionysistemas.com>
2025-08-12 17:52:34 -0500

    se agrego ruta render

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 10000,
+    allowedHosts: ['crudlaptops-fronted.onrender.com'],
     proxy: {
       '/api': 'http://127.0.0.1:8000'
     }
